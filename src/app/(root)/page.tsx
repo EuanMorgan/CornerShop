1b9c5c41a1d386619a7719ee61636007acc3da82
Euan Morgan <euan@carboncode.uk>
2023-07-04 13:11:59 +0100

    feat: modals

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,17 @@
-import {UserButton} from '@clerk/nextjs';
-import {Button} from '~/components/ui/button';
+'use client';
+
+import {useEffect} from 'react';
+
+import {useStoreModal} from '~/hooks/use-store-modal';
 
 export default function SetupPage() {
-  return (
-    <div className='p-4'>
-      <p>This is a protected route</p>
-      <UserButton afterSignOutUrl='/' />
-    </div>
-  );
+  const onOpen = useStoreModal(state => state.onOpen);
+  const isOpen = useStoreModal(state => state.isOpen);
+  useEffect(() => {
+    // Ensure modal is open, the user must create a store
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className='p-4'>Root page</div>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
-import './globals.css';
-import {Inter} from 'next/font/google';
 import {ClerkProvider} from '@clerk/nextjs';
+import {Inter} from 'next/font/google';
+
+import {ModalProvider} from '~/providers/modal-provider';
+
+import './globals.css';
+
 const inter = Inter({subsets: ['latin']});
 
 export const metadata = {
@@ -12,7 +16,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
